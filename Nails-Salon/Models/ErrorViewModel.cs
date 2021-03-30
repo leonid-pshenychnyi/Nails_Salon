492cad523ac29f0d41f1aeb50494c3fc2d5f55e0
DESKTOP-O840JJ8\peron <peronov.llu@gmail.com>
2021-03-30 17:57:35 +0300

    add c#

---
+++ Nails-Salon/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Nails_Salon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
